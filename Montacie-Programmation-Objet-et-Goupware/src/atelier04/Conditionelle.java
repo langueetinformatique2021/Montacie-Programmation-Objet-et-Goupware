4829ef629c0b5020ad8c8b5b94406d11f481f98c
isha <isha@fedora>
2021-10-21 09:52:30 +0200

    Squelette pour l'exercice 1

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/Conditionelle.java
@@ -0,0 +1,12 @@
+package atelier04;
+
+public class Conditionelle {
+
+	public static void main(String[] args) {
+		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+		
+		System.out.println(p4);
+
+	}
+
+}
