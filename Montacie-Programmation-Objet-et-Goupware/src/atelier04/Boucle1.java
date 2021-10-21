a47cc1b5658b409ead9e03400c8f55c3b01b70e0
isha <isha@fedora>
2021-10-21 10:56:37 +0200

    corrigé de Boucle1

--- Montacie-Programmation-Objet-et-Goupware/src/atelier04/Boucle1.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/Boucle1.java
@@ -3,13 +3,23 @@ package atelier04;
 public class Boucle1 {
 
 	public static void main(String[] args) {
-		String s1 = "azerty", s2 = "ytreza";
+		String s1 = "azerty", s2 = "ytrezb";
 		int i1 = 0, i2 = s2.length() - 1;
 		char c1, c2;
-		boolean flag;
+		boolean flag = false;
 
-		c1 = s1.charAt(i1);
-		c2 = s2.charAt(i2);
+		if (s1.length() == s2.length()) {
+			int i, n = s1.length();
+			for (i = 0; i < n; i++) {
+				if (s1.charAt(i) != s2.charAt(n - i - 1))
+					break;
+			}
+			if (i == n)
+				flag = true;
+
+		}
+
+		System.out.println(flag);
 	}
 
 }
