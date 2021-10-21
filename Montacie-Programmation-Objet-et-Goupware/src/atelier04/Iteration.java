84ee52639f0f1f253dad57b344556bbf1ccfb966
isha <isha@fedora>
2021-10-21 11:22:50 +0200

    corrigé d'Iteration

--- Montacie-Programmation-Objet-et-Goupware/src/atelier04/Iteration.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/Iteration.java
@@ -3,15 +3,16 @@ package atelier04;
 public class Iteration {
 
 	public static void main(String[] args) {
-		String[] prenoms = {"Christian", "Karen", "Victoria", "Agatha", "Philippe", "Djamé"};
-		
-		int imin;
-		
-		prenoms[0].compareTo(prenoms[1]);
-		
-		
-		
-		
+		String[] prenoms = { "Christian", "Karen", "Victoria", "Agatha", "Philippe", "Djamé" };
+
+		int imin = 0;
+		for (int i = 1; i < prenoms.length; i++) {
+			if (prenoms[i].compareTo(prenoms[imin]) < 0)
+				imin = i;
+
+		}
+		System.out.println(prenoms[imin]);
+
 	}
 
 }
