dc333384d78584a4370bf4f4f94ab3aecdac69fd
isha <isha@fedora>
2021-10-21 09:59:31 +0200

    Corrigé de Conditionnelle 1

--- Montacie-Programmation-Objet-et-Goupware/src/atelier04/Conditionnelle.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/Conditionnelle.java
@@ -1,9 +1,11 @@
 package atelier04;
 
-public class Conditionelle {
+public class Conditionnelle {
 
 	public static void main(String[] args) {
 		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+		
+		
 		
 		System.out.println(p4);
 
