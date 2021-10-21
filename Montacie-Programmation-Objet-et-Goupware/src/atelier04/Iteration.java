0301108e61949e27ea7db41f8a9dc08f3565afdf
isha <isha@fedora>
2021-10-21 11:12:42 +0200

    Squelette d'Iteration

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/Iteration.java
@@ -0,0 +1,17 @@
+package atelier04;
+
+public class Iteration {
+
+	public static void main(String[] args) {
+		String[] prenoms = {"Christian", "Karen", "Victoria", "Agatha", "Philippe", "Djamé"};
+		
+		int imin;
+		
+		prenoms[0].compareTo(prenoms[1]);
+		
+		
+		
+		
+	}
+
+}
