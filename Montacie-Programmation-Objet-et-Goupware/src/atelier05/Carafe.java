a2e89de1242de1adf1ccc81f70e48b6bc833e887
isha <isha@fedora>
2021-10-28 11:02:51 +0200

    méthodes publiques de carafe Vider et Remplir

--- Montacie-Programmation-Objet-et-Goupware/src/atelier05/Carafe.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/Carafe.java
@@ -3,7 +3,7 @@ package atelier05;
 public class Carafe {
 	private int contenu_, capacite_;
 	
-	/** creation d'une carafe vide d'une capacite donne
+	/** creation d'une carafe vide d'une capacite donnee
 	 * 
 	 * @param capacite capacite de la carafe en litres
 	 */
@@ -12,7 +12,7 @@ public class Carafe {
 		contenu_ = 0;
 	}
 	
-	/** creation d'une carafe d'une capacite donne et contenant une quantite d'eau
+	/** creation d'une carafe d'une capacite donnee et contenant une quantite d'eau
 	 * 
 	 * @param capacite capacite de la carafe en litres
 	 * @param contenu quantite d'eau en litres conenue dans la carafe
@@ -23,5 +23,16 @@ public class Carafe {
 			contenu_ = capacite;
 		else contenu_ = contenu;
 	}
+	
+	/** remplir completement une carafe */
+	public void Remplir() {
+		contenu_ = capacite_;
+	}
+	
+	/** vider completement une carafe */
+	public void Vider() {
+		contenu_ = 0;
+	}
+	
 
 }
