f75031591d5c0858c27ddfdc05145af228f86849
isha <isha@fedora>
2021-10-28 10:35:48 +0200

    correction exercice 1

--- Montacie-Programmation-Objet-et-Goupware/src/atelier05/testFete.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/testFete.java
@@ -4,15 +4,18 @@ public class testFete {
 
 	/** calcul du nombre de jours entre aujourd'hui et le 14 juillet 2022 */
 	public static void main(String[] args) {
-		Date today = new Date(28,10,2021);
-		Date fete = new Date(14,7,2022);
-		
+		Date today = new Date(28, 10, 2021);
+		Date fete = new Date(14, 7, 2022);
+
 		// on incrémente today tant que cette date n'est pas égale à fete
-		
-		today.Incrementer();
-		
-		today.CompareTo(fete);
-		
+
+		int nbJours = 0;
+		while (today.CompareTo(fete) == true) {
+			today.Incrementer();
+			nbJours++;
+		}
+
+		System.out.println("Le nombre de jours est égal à " + nbJours);
 
 	}
 
