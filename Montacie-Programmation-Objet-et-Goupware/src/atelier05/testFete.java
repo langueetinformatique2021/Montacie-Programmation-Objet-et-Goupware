beb122fc48ec485e81ad9b2051a1e108dd3f2dbe
isha <isha@fedora>
2021-10-28 10:17:49 +0200

    debut de la correction de l'exercie 1

--- Montacie-Programmation-Objet-et-Goupware/src/atelier05/testFete.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/testFete.java
@@ -3,8 +3,9 @@ package atelier05;
 public class testFete {
 
 	/** calcul du nombre de jours entre aujourd'hui et le 14 juillet 2022 */
-	 
 	public static void main(String[] args) {
+		Date today = new Date(28,10,2021);
+		Date fete = new Date(14,7,2022);
 		
 
 	}
