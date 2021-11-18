ae783c51525cf5403b3555f22c5e0d59b88529cb
isha <isha@fedora>
2021-11-18 11:17:16 +0100

    Squelette de la classe Chat

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/Chat.java
@@ -0,0 +1,17 @@
+package atelier06;
+
+public class Chat extends Félin implements Domesticable {
+
+	@Override
+	public void domestiquer(String nom) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String nom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
