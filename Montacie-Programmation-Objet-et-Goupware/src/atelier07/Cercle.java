3fd23a123ca78a69f69d931295c8dbb2567d9418
isha <isha@fedora>
2021-11-25 10:41:16 +0100

    Squelette de la classe Cercle

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/Cercle.java
@@ -0,0 +1,19 @@
+package atelier07;
+
+import cours04.FigureGeometrique;
+
+public class Cercle extends FigureGeometrique implements Comparable<T> {
+
+	@Override
+	public float perimetre() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float surface() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
