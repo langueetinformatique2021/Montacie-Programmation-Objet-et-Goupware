8b8e60e7f3b82ea14411fae0ed64e56a8cf19550
isha <isha@fedora>
2021-11-25 10:47:24 +0100

    Cercle avec l'interface de base Comparable

--- Montacie-Programmation-Objet-et-Goupware/src/atelier07/Cercle.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/Cercle.java
@@ -2,7 +2,13 @@ package atelier07;
 
 import cours04.FigureGeometrique;
 
-public class Cercle extends FigureGeometrique implements Comparable<T> {
+public class Cercle extends FigureGeometrique implements Comparable {
+
+	@Override
+	public int compareTo(Object arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 	@Override
 	public float perimetre() {
