8040d809184e1fddf8e8ac039dd39be9ba4de68a
isha <isha@fedora>
2021-12-02 11:05:20 +0100

    correction de E421

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier08/E421.java
@@ -0,0 +1,9 @@
+package atelier08;
+
+public class E421 extends Exception {
+	
+	public E421(String joueur) {
+		super(joueur);
+	}   
+
+}
---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier08/testE421.java
@@ -0,0 +1,25 @@
+package atelier08;
+
+public class testE421 {
+
+	public static void main(String[] args) {
+		Des d1 = new Des();
+		Des d2 = new Des();
+		Des d3 = new Des();
+		try {
+			while (true)
+				lancerDes421(d1, d2, d3);
+		}
+
+		catch (E421 e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public static void lancerDes421(Des d1, Des d2, Des d3) throws E421 {
+		if ((d1.lancer() == 4) && (d2.lancer() == 2) && (d3.lancer() == 1))
+			throw new E421("gagnant");
+
+	}
+
+}
