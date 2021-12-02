46b8f724ca9507adf59fbf11cf5a4b5b8e4c72fe
isha <isha@fedora>
2021-12-02 10:07:44 +0100

    division3

--- Montacie-Programmation-Objet-et-Goupware/src/atelier08/division3.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier08/division3.java
@@ -4,12 +4,13 @@ public class division3 {
 	public static void main(String[] args) {
 		division();
 	}
+
 	public static void division() {
-			int i = 1000, j;
-			do {
-				i--;
-				j = 1 / i;
-			} while (true);
-		
+		int i = 1000, j;
+		do {
+			i--;
+			j = 1 / i;
+		} while (true);
+
 	}
 }
