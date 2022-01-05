36f32526c27c75e2d475f017745e5227f7b0e66b
Montacie <Claude.Montacie@Paris-Sorbonne.fr>
2022-01-05 14:41:40 +0100

    examen

---
+++ Montacie-Programmation-Objet-et-Goupware/src/examen/A.java
@@ -0,0 +1,24 @@
+package examen;
+public abstract class A {
+	char d[];
+	static int n;
+
+	A(char b, char c, int m) {
+		n = m;
+		d = new char[n];
+		for (int i = 0; i < n; i++) {
+			if ((i % 2) == 0)
+				d[i] = b;
+			else
+				d[i] = c;
+		}
+	}
+
+	abstract public void m1(int m);
+
+	public void m2(int m) {
+		for (int i = 0; i < n; i += m) {
+			System.out.println(d[i]);
+		}
+	}
+}
---
+++ Montacie-Programmation-Objet-et-Goupware/src/examen/B.java
@@ -0,0 +1,23 @@
+package examen;
+public class B extends A {
+	static String a_initialiser = "claude"; // à remplacer par le prénom de l'étudiant
+
+	B(char b, char c, int m) {
+		super(b, c, m);
+		for (int i = 0; i < n; i++)
+			a_initialiser += d[i];
+	}
+
+	public void m1(int m) {
+		for (int i = 0; i < n; i++) {
+			System.out.println(d[i]); } 
+	}
+
+
+	public static void main(String[] args) {
+		B b = new B('h', a_initialiser.charAt(2), 6);
+		b.m1(3);
+		b.m2(4);
+	}
+
+	}
