5729c9d252147819c40cf27ba572ac39b475c934
isha <isha@fedora>
2022-10-06 10:34:49 +0200

    devoir n°1

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/condition4.java
@@ -0,0 +1,17 @@
+/** devoir n°1 à faire pour le 20 octobre avant 9h30 sur votre répertoire github dans un package
+ *  devoir1
+ *  aucun mail ne sera accepté 
+ * 
+ */
+
+
+package atelier04;
+
+public class condition4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
