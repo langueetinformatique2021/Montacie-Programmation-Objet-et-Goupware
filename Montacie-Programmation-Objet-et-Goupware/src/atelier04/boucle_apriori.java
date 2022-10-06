1115be5f155ab1d40a6cb627f4681881be3aa3ac
isha <isha@fedora>
2022-10-06 10:39:31 +0200

    squelette de boucle_apriori

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/boucle_apriori.java
@@ -0,0 +1,18 @@
+package atelier04;
+
+public class boucle_apriori {
+
+	public static void main(String[] args) {
+
+		String s1 = "azerty", s2 = "ytreza";
+		int i1 = 0, i2 = s2.length() - 1;
+
+		char c1, c2;
+		boolean flag;
+
+		c1 = s1.charAt(i1);
+		c2 = s2.charAt(i2);
+
+	}
+
+}
