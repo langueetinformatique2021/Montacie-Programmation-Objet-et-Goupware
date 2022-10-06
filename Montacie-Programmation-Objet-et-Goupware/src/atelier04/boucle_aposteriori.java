0091fb45d786f418406ce869547cc6911601d11f
isha <isha@fedora>
2022-10-06 11:04:55 +0200

    squelette de la boucle a posteriori

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/boucle_aposteriori.java
@@ -0,0 +1,20 @@
+package atelier04;
+
+public class boucle_aposteriori {
+
+	public static void main(String[] args) {
+
+		String s1 = "azerty", s2 = "ytreza";
+		int i1 = 0, i2 = s2.length() - 1;
+
+		
+		if (s1.length() == s2.length()) {
+			do {
+				
+			}
+			while () 
+		
+
+	}
+
+}
