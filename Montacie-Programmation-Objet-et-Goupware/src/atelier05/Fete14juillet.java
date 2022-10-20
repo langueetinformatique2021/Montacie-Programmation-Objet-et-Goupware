8ff02c729f32bc2db3ae2b15008f1607bbbcbd90
isha <isha@fedora>
2022-10-20 09:40:58 +0200

    calcul du nombre de jours avant le 14 juillet 2023

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/Fete14juillet.java
@@ -0,0 +1,18 @@
+package atelier05;
+
+import cours04.Date;
+
+/** calcul du nombre de jours avant le 14 juillet 2023
+ * 
+ * @author isha
+ *
+ */
+
+public class Fete14juillet {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
