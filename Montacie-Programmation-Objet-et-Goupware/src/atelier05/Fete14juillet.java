14cea1fbbd9b82497e125926b871fadaccdd0655
isha <isha@fedora>
2022-10-20 10:00:19 +0200

    Corrigé Fete 14 juillet

--- Montacie-Programmation-Objet-et-Goupware/src/atelier05/Fete14juillet.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/Fete14juillet.java
@@ -2,7 +2,8 @@ package atelier05;
 
 import cours04.Date;
 
-/** calcul du nombre de jours avant le 14 juillet 2023
+/**
+ * calcul du nombre de jours avant le 14 juillet 2023
  * 
  * @author isha
  *
@@ -11,8 +12,16 @@ import cours04.Date;
 public class Fete14juillet {
 
 	public static void main(String[] args) {
-		
+		Date today = new Date(20, 10, 2022);
+		Date fete14 = new Date(14, 7, 2023);
 
+		int nJours = 0;
+		do {
+			today.Incrementer();
+			nJours++;
+		} while (today.CompareTo(fete14));
+		
+		System.out.println(nJours);
 	}
 
 }
