f550573fbe886a3853fa4d0f026103d9d899ab8b
isha <isha@fedora>
2022-11-17 10:33:19 +0100

    squelette de Calendar2

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/Calendar2.java
@@ -0,0 +1,22 @@
+package atelier07;
+
+import java.util.GregorianCalendar;
+
+/** dérivée de Calendar implémentant un nouvel attribut cours 
+ * et trois nouvelles méthodes creation() afficher() et duree() */
+public class Calendar2 extends GregorianCalendar {
+	
+	private boolean cours;
+	
+	/** affiche la date et l’heure de la création de l’objet */
+	public void creation() {}
+	
+	/** affiche la date et l’heure courante */
+	public void afficher() {}
+	
+	/** affiche le nombre de secondes depuis la création de l’objet */
+	public void duree() {}
+	
+	
+
+}
