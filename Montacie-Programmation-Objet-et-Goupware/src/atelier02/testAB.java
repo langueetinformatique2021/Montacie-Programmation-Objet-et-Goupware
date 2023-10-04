3e0163bf5565322e62dc3701aef5c66b0931a501
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:40:46 +0200

    Update testAB.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier02/testAB.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier02/testAB.java
@@ -11,11 +11,7 @@ public class testAB {
 
 	public static void main(String[] args) {
 
-		System.out.println(A.treize(10));
-		System.out.println(A.treize(27));
-		System.out.println(B.divise(10));
-		System.out.println(B.divise(27));
-
+	
 	}
 
 }
