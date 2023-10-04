ede7af466160494bc1b1264f207e5701d3131d79
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:48:38 +0200

    Update PileFace.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier08/PileFace.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier08/PileFace.java
@@ -2,19 +2,7 @@ package atelier08;
 
 public class PileFace {
 	public static void main(String[] args) {
-		Aleat pileface = new Aleat(2);
-
-		int pile = 0, face = 0;
-		for (int i = 0; i < 1000; i++) {
-			int tire = pileface.get();
-			System.out.print(tire);
-			if (tire == 1)
-				pile++;
-			else
-				face++;
-		}
-		System.out.println();
-		System.out.println(pile + " piles et " + face + " faces");
+		
 
 	}
 }
