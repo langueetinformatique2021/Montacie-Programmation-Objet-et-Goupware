b824d10cc8814983f1d833c70f34eda940e4c570
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:51:04 +0200

    Update testLireTouUnTexte.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier10/testLireTouUnTexte.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier10/testLireTouUnTexte.java
@@ -7,11 +7,7 @@ public class testLireTouUnTexte {
 
 	public static void main(String[] args) {
 		
-		LireToutUnTexte lt = new LireToutUnTexte(new File("livres/adelaide.txt"));
-		//lt.afficher();
-		System.out.println(lt.Chercher("Rothbanner"));
 		
-		System.out.println(lt.ChercherVoir("Rothbanner", 10));
 	}
 
 }
