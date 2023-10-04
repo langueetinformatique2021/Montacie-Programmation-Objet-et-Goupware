54320e4088927f95e6be636cf9d2a0287696a348
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:46:38 +0200

    Update Calendar2.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier07/Calendar2.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/Calendar2.java
@@ -7,36 +7,7 @@ import java.util.GregorianCalendar;
  * et trois nouvelles méthodes creation() afficher() et duree() */
 public class Calendar2 extends GregorianCalendar {
 	
-	private boolean cours;
 	
-	public Calendar2() {
-		Date date = new Date();
-		this.setTime(date);	
-	}
-	
-	/** affiche la date et l’heure de la création de l’objet */
-	public void creation() {
-		System.out.println("TIME: " + this.getTime());
-	}
-	
-	/** affiche la date et l’heure courante */
-	public void afficher() {
-		Date date = new Date();
-		GregorianCalendar calendar = new GregorianCalendar();
-		calendar.setTime(date);	
-		System.out.println("TIME: " + calendar.getTime());
-	}
-	
-	/** affiche le nombre de secondes depuis la création de l’objet */
-	public void duree() {
-		Date date = new Date();
-		GregorianCalendar calendar = new GregorianCalendar();
-		calendar.setTime(date);	
-		long nbSeconde = (calendar.getTimeInMillis()- this.getTimeInMillis()) / 1000;
-		System.out.println("nbSeconde " + nbSeconde);
-		
-		
-	}
 	
 	
 
