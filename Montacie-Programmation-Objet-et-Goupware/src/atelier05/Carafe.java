0bfa4a81318b878a72665482580ec1e3374fef2d
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:43:22 +0200

    Update Carafe.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier05/Carafe.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/Carafe.java
@@ -2,7 +2,7 @@ package atelier05;
 
 /** gestion du problème des carafes */
 public class Carafe {
-	private int contenu_, capacite_;
+	
 
 	/**
 	 * Contructeur non vide
@@ -10,67 +10,43 @@ public class Carafe {
 	 * @param contenu  contenu en litres de la carafe
 	 * @param capacite capacite en litres de la carafe
 	 */
-	public Carafe(int contenu, int capacite) {
-		contenu_ = contenu;
-		capacite_ = capacite;
-	}
+	
 
 	/**
 	 * vider la carafe accesseur en écriture
 	 */
-	public void vider() {
-		contenu_ = 0;
-	}
-
+	
 	/**
 	 * remplir la carafe accesseur en écriture
 	 */
-	public void remplir() {
-		contenu_ = capacite_;
-	}
+	
 
 	/**
 	 * accesseur en lecture
 	 * 
 	 * @return le contenu de la carafe
 	 */
-	public int contenu() {
-		return contenu_;
-	}
+	
 
 	/**
 	 * accesseur en lecture
 	 * 
 	 * @return la capacite de la carafe
 	 */
-	public int capacite() {
-		return capacite_;
-	}
+	
 
 	/**
 	 * transvaser mon contenu dans une autre carafe
 	 * 
 	 * @param c autre carafe
 	 */
-	public void transvaser(Carafe c) {
-		int reste = c.capacite() - c.contenu();
-		if (reste > this.contenu()) {
-			c.contenu(c.contenu() + this.contenu());
-			this.vider();
-		} else {
-			this.contenu(this.contenu() - reste);
-			c.remplir();
-		}
-	}
+	
 
 	/**
 	 * accesseur en écriture privé
 	 * 
 	 * @param c nouveau contenu de la carafe
 	 */
-	private void contenu(int c) {
-		contenu_ = c;
-
-	}
+	
 
 }
