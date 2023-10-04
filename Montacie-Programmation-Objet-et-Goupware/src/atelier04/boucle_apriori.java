688795f9073dbb769f39d832802abc761179379d
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:41:29 +0200

    Update boucle_apriori.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier04/boucle_apriori.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/boucle_apriori.java
@@ -4,17 +4,7 @@ public class boucle_apriori {
 
 	public static void main(String[] args) {
 
-		String s1 = "azerty", s2 = "ytreza";
-		int i1 = 0, i2 = s2.length() - 1;
-
-		
-		if (s1.length() == s2.length()) {
-			while ((i2 >= 0) && (s1.charAt(i1) == s2.charAt(i2))) {
-				i1++;
-				i2--;
-			}
-			if (i2 == -1)
-				System.out.println("mot miroir");
+	
 			
 		}
 		
