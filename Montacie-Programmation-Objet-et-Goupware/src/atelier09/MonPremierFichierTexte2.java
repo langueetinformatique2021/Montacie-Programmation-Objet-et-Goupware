6624c30ab0c02eb34ed50a4b7ce4c0519a1ef331
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:50:22 +0200

    Update MonPremierFichierTexte2.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier09/MonPremierFichierTexte2.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/MonPremierFichierTexte2.java
@@ -8,36 +8,6 @@ import java.io.BufferedReader;
 
 public class MonPremierFichierTexte2 {
 
-	public static void main(String[] args) {
-
-		try {
-			PrintWriter pr = new PrintWriter(
-					new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/Nom.txt"));
-			pr.println("Victoria Eyhrabide");
-			pr.println("Claude Montacié");
-			pr.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println(Verifier());
-
-	}
-
-	public static boolean Verifier() {
-		try {
-			BufferedReader br = new BufferedReader(
-					new FileReader(new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/Nom.txt")));
-			if (br.readLine().equals("Victoria Eyhrabide") == false)
-				return false;
-			if (br.readLine().equals("Claude Montacié") == false)
-				return false;
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return true;
-		
-	}
+	
 
 }
