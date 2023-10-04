8741e92e371ff40206c61aefdd7607793376ba7c
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:43:45 +0200

    Update Fete14juillet.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier05/Fete14juillet.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/Fete14juillet.java
@@ -12,16 +12,7 @@ import cours04.Date;
 public class Fete14juillet {
 
 	public static void main(String[] args) {
-		Date today = new Date(20, 10, 2022);
-		Date fete14 = new Date(14, 7, 2023);
-
-		int nJours = 0;
-		do {
-			today.Incrementer();
-			nJours++;
-		} while (today.CompareTo(fete14));
 		
-		System.out.println(nJours);
 	}
 
 }
