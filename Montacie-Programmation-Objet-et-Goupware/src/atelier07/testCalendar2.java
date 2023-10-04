259f94c93cd8793886ffe635f31f01df02e66c4c
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:46:52 +0200

    Update testCalendar2.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier07/testCalendar2.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/testCalendar2.java
@@ -5,12 +5,7 @@ import util.Keyboard;
 public class testCalendar2 {
 
 	public static void main(String[] args) {
-		Calendar2 c = new Calendar2();
-		c.creation();
-		Keyboard.getInt("attente");
-		c.creation();
-		c.afficher();
-		c.duree();
+		
 		
 	}
 
