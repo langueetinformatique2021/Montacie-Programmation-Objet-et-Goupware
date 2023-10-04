0f69098ad1b0a0090192350e0421acf4bf045fd7
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:49:36 +0200

    Update Copie.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier09/Copie.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/Copie.java
@@ -9,21 +9,7 @@ public class Copie {
 
 	public static void main(String[] args) {
 
-		try {
-			File fin = new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/Nom.txt");
-			FileInputStream fsource = new FileInputStream(fin);	
-			byte[] tmp = new byte[(int)fin.length()];
-			fsource.read(tmp);
-			fsource.close();
-			
-			FileOutputStream fcible = new FileOutputStream(
-					new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/NomC.txt"));
-			fcible.write(tmp);
-			fcible.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 }
