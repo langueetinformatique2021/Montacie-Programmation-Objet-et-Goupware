47c2c7f8368a519e65dbb95d60fb9439ced478cb
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:48:25 +0200

    Update LancerDes.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier08/LancerDes.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier08/LancerDes.java
@@ -3,13 +3,12 @@ package atelier08;
 public class LancerDes extends Aleat {
 	
 	public LancerDes() {
-		super(6);
+		
 		
 	}
 	
 	public TroisDes lancer() {
-		TroisDes tire = new TroisDes(super.get(), super.get(), super.get());
-		return tire;
+		
 		
 	}
 
