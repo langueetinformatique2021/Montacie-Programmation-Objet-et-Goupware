07e7cfdc167570478ba4838988caa0da822170ce
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:40:25 +0200

    Update PairImpair.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier02/PairImpair.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier02/PairImpair.java
@@ -11,29 +11,19 @@ public class PairImpair {
 	 * 
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// test de pair et impair
-		System.out.println(pair(3));
-		System.out.println(impair(4));
-		
-
-	}
-	/**
-	 * 
-	 * @param v
-	 * @return
-	 */
-	public static boolean pair(int v) {
-		return ((v % 2) == 0);
-	}
 	
 	/**
 	 * 
 	 * @param v
 	 * @return
 	 */
-	public static boolean impair (int v) {
-		return !pair(v);
-	}
+	
+	
+	/**
+	 * 
+	 * @param v
+	 * @return
+	 */
+	
 
 }
