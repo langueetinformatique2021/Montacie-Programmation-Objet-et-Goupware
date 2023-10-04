22e69464ec3bcbd967a1c8ae88dc664d3f1db441
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:49:20 +0200

    Update Compresse.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier09/Compresse.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/Compresse.java
@@ -11,24 +11,6 @@ public class Compresse {
 
 	public static void main(String[] args) {
 
-		try {
-			File fin = new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/Nom.txt");
-			FileInputStream fsource = new FileInputStream(fin);	
-			byte[] tmp = new byte[(int)fin.length()];
-			fsource.read(tmp);
-			fsource.close();
-			
-	
-			FileOutputStream fout = new FileOutputStream(
-					new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/Nom.txt.zip"));
-			DataOutputStream fcible = new DataOutputStream(new DeflaterOutputStream(fout));
-			
-			fcible.write(tmp);
-			fcible.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
