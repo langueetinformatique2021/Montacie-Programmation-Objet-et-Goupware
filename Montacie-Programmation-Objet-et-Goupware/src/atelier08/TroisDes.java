a09402b225db76a3109e83d1999c2a14f3cd924f
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:48:57 +0200

    Update TroisDes.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier08/TroisDes.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier08/TroisDes.java
@@ -1,12 +1,9 @@
 package atelier08;
 
 public class TroisDes {
-	private int d1,d2,d3;
 	
 	public TroisDes(int d1, int d2, int d3) {
-		this.d1 = d1;
-		this.d2 = d2;
-		this.d3 = d3;
+		
 	}
 	
 	
