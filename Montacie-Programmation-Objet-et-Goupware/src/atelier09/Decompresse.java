d284e9e1365266be39a2a2dc54f88264b1daade4
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:49:51 +0200

    Update Decompresse.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier09/Decompresse.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/Decompresse.java
@@ -16,25 +16,7 @@ public class Decompresse {
 
 	public static void main(String[] args) {
 
-		try {
-			FileInputStream fin = new FileInputStream(
-					new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/Nom.txt.zip"));
-			DataInputStream fsource = new DataInputStream(new InflaterInputStream(fin));
-			FileOutputStream fcible = new FileOutputStream(
-					new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/NomD.txt"));
-
-			int tmp;
-			while ((tmp = fsource.read()) != -1) {
-				fcible.write(tmp);
-
-			}
-			
-			fsource.close();
-			fcible.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
 	}
 
 }
