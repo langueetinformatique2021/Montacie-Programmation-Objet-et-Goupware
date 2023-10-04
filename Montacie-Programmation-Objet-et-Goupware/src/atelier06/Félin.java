7b0e14b4f9436ac298026b5ad03bb247490b65ed
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:45:45 +0200

    Update Félin.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier06/Félin.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/Félin.java
@@ -9,20 +9,18 @@ public class Félin extends Animal {
 	 * le constructeur de Félins fait appel au constructeur de la sur-classe Animal
 	 */
 	public Félin(String type) { // les félins ont 4 pattes
-		super(type, 4);
+	
 	}
 
 	/** présentation des caractéristiques du félin */
 	public void présente() {
 		// appel de la méthode de la sur-classe
-		super.présente();
-		String etat = (domestique) ? "domestique" : "sauvage";
-		System.out.println("je suis vraiment un animal " + etat);
+		
 	}
 
 	/** cri du félin */
 
 	public void crie() {
-		System.out.println("je rugis");
+		
 	}
 }
