20ab6bf7d189a3208a383a03c60602c35b65d72d
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:50:07 +0200

    Update MonPremierFichierTexte.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier09/MonPremierFichierTexte.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/MonPremierFichierTexte.java
@@ -8,14 +8,7 @@ public class MonPremierFichierTexte {
 
 	public static void main(String[] args) {
 
-		try {
-			PrintWriter pr = new PrintWriter(new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/Nom.txt"));
-			pr.println("Victoria Eyhrabide");
-			pr.println("Claude Montacié");
-			pr.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		
 
 	}
 
