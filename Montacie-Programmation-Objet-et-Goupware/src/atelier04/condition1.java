eda3c7b5ea93c74ea270d5480e85769cf7f7d335
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:41:48 +0200

    Update condition1.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier04/condition1.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/condition1.java
@@ -4,26 +4,7 @@ public class condition1 {
 
 	public static void main(String[] args) {
 		
-		int p1 = 10, p2 = 11, p3 = 12, p4 = 4, p5 = 8;
 		
-		if (p1 > p2) {
-			if (p3 > p1)
-				p4 = p3;
-			else 
-				p4 = p1;
-		}
-		else {
-			
-			if (p3 > p2)
-				p4 = p3;
-			else 
-				p4 = p2;
-			
-		}
-		
-		
-		
-		System.out.println(p4) ;
 		
 
 	}
