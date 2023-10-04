4c48919aa49814a6a8b05dd8e0f17f3e5588cd1d
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:48:07 +0200

    Update Division3.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier08/Division3.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier08/Division3.java
@@ -2,25 +2,7 @@ package atelier08;
 
 public class Division3 {
 
-	public static void main(String[] args) {
-
-		try {
-			division();
-
-		} catch (ArithmeticException e) {
-			System.out.println(e.getMessage());
-		}
-
-	}
-
-	public static void division() throws ArithmeticException {
-		int i = 1000, j;
-		do {
-			i--;
-			j = 1 / i;
-		} while (true);
-	}
-
+	
 	
 
 }
