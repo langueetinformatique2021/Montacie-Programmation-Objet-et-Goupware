1b5bdc1fe8d91541eddf9e60b08dd2e5c69ba6ad
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:47:44 +0200

    Update Division2.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier08/Division2.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier08/Division2.java
@@ -3,12 +3,7 @@ public class Division2 {
 	public static void main(String[] args) { division(); }
 
 	public static void division() throws ArithmeticException {
-		try {
-		int i = 1000, j;
-		do {i--; j = 1 / i;} while (true);
-		} catch (ArithmeticException e){
-			System.out.println(e.getMessage());
-		}
+		
 	}
 
 }
