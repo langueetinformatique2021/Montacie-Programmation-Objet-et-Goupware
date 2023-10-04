0c20084c098d3ff656b341383299454103c2a1ba
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:47:07 +0200

    Update testDate.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier07/testDate.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/testDate.java
@@ -7,18 +7,7 @@ public class testDate {
 
 	public static void main(String[] args) {
 
-		GregorianCalendar calendar = new GregorianCalendar();
-		Date date = new Date();
-
-		calendar.setTime(date);
-
-		System.out.println("TIME: " + calendar.getTime());
-		System.out.println("YEAR: " + calendar.get(calendar.YEAR));
-		System.out.println("MONTH: " + calendar.get(calendar.MONTH));
-		System.out.println("DATE: " + calendar.get(calendar.DATE));
-
-		System.out.println("DAY_OF_YEAR: " + calendar.get(calendar.DAY_OF_YEAR));
-		System.out.println("HOUR_OF_DAY: " + calendar.get(calendar.HOUR_OF_DAY));
+		
 
 	}
 
