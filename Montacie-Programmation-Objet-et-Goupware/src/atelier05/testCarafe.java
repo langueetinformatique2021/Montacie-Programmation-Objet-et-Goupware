6fadde6b910b39ec1586a8a1c3300e57181289b4
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:44:03 +0200

    Update testCarafe.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier05/testCarafe.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/testCarafe.java
@@ -3,19 +3,7 @@ package atelier05;
 public class testCarafe {
 
 	public static void main(String[] args) {
-		Carafe c1 = new Carafe (0, 7);
-		Carafe c2 = new Carafe (0, 5);
 		
-		c1.remplir();
-		c2.vider();
-		
-		System.out.println(c1.contenu());
-		System.out.println(c2.contenu());
-		
-		c1.transvaser(c2);
-		
-		System.out.println(c1.contenu());
-		System.out.println(c2.contenu());
 	
 	}
 
