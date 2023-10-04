0dd2126e3283c6ab1a42cdfb551eeb3bf3b073ba
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:46:01 +0200

    Update testChat.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier06/testChat.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/testChat.java
@@ -3,11 +3,7 @@ package atelier06;
 public class testChat {
 
 	public static void main(String[] args) {
-		Chat ch = new Chat();
-		ch.domestiquer("Filou");
-		ch.présente();
-		ch.crie();
-
+		
 	}
 
 }
