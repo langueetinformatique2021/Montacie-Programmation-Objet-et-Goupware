8cb7cad1754e20c15236e0d10f23d6179ffbd2f5
langueetinformatique2021 <92399827+langueetinformatique2021@users.noreply.github.com>
2023-10-04 23:45:02 +0200

    Update Chat.java

--- Montacie-Programmation-Objet-et-Goupware/src/atelier06/Chat.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/Chat.java
@@ -2,28 +2,8 @@ package atelier06;
 
 public class Chat extends Félin implements Domesticable {
 	
-	private String nom;
 	
-	public Chat() { 
-		super("chats");
-	}
 
-	@Override
-	public void domestiquer(String nom) {
-		domestique = true;
-		this.nom = nom;
-
-	}
-
-	@Override
-	public String nom() {
-		return this.nom;
-	}
 	
-	public void crie() {
-		if (domestique == false) super.crie();
-		else 
-			System.out.println("Je miaule et je m'appelle "  + this.nom());
-	}
 
 }
