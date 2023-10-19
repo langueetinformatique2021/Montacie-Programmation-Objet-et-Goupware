1482123da8be7b9facd330fcef87029b32d45243
isha <isha@fedora>
2023-10-19 11:57:48 +0200

    correction

--- Montacie-Programmation-Objet-et-Goupware/src/atelier04/Conditionnelle1.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier04/Conditionnelle1.java
@@ -1,14 +1,28 @@
 package atelier04;
 
-/** Ajout des instructions structurées par des branchements conditionnels permettant
-d’obtenir le maximum dess variables p1, p2, p3 dans la variable p4. */
+/**
+ * Ajout des instructions structurées par des branchements conditionnels
+ * permettant d’obtenir le maximum des variables p1, p2, p3 dans la variable p4.
+ */
 
 public class Conditionnelle1 {
-	
-	
 
 	public static void main(String[] args) {
-		int p1 = 10, p2 = 9, p3 = -1, p4 = 4, p5 = 8;
+		int p1 = 10, p2 = 11, p3 = 12, p4 = 4, p5 = 8;
+
+		if (p1 > p2) {
+			if (p1 > p3)
+				p4 = p1;
+			else
+				p4 = p3;
+		} else {
+			if (p2 > p3)
+				p4 = p2;
+			else
+				p4 = p3;
+		}
+		
+		System.out.println(p4);
 
 	}
 
