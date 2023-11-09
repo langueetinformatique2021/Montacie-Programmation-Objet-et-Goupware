a51f25ee192f850b4fa3c2d2522105664dcdbe2f
isha <isha@fedora>
2023-11-09 11:05:05 +0100

    Squelette de Carafe

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/Carafe.java
@@ -0,0 +1,32 @@
+package atelier05;
+
+/**
+ * On propose de considérer la carafe comme un objet ayant deux attributs privés
+ * (contenu_ et capacité_) et l’interface publique suivante reprenant les
+ * opérations ci dessus (Remplir, Vider, Contenu, Capacité et Transvaser)
+ */
+
+public class Carafe {
+	private int contenu_, capacité_;
+
+	public void Remplir() {
+
+	}
+
+	public void Vider() {
+
+	}
+
+	public void Contenu() {
+
+	}
+
+	public void Capacité() {
+
+	}
+
+	public void Transvaser() {
+
+	}
+
+}
