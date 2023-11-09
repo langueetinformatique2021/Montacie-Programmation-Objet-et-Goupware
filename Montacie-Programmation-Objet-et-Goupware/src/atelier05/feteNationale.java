72d719d1ad24e7fbe769dbd979593882db319911
isha <isha@fedora>
2023-11-09 10:51:19 +0100

    correction 14 juillet

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/feteNationale.java
@@ -0,0 +1,24 @@
+package atelier05;
+
+public class feteNationale {
+
+	public static void main(String[] args) {
+		
+		// Instanciation de deux objets de type date
+		Date today = new Date(9, 11, 2023); 
+		//Date dfin = new Date(10, 11, 2023);
+		Date dfin = new Date(14, 7, 2024);
+		int njours = 0;
+
+		today.Afficher(); dfin.Afficher();
+		do { today.Incrementer(); njours ++; }
+		while (today.CompareTo(dfin));
+		
+		System.out.println(njours + " jours");
+		}
+	
+	
+
+	}
+
+
