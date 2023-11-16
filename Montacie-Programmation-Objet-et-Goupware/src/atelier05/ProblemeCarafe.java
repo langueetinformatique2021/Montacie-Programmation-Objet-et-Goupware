1156f8bc811c1bde2336ae4752a4b4b72cda205f
isha <isha@fedora>
2023-11-16 11:56:06 +0100

    Problème des carafes

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier05/ProblemeCarafe.java
@@ -0,0 +1,16 @@
+package atelier05;
+
+public class ProblemeCarafe {
+
+	public static void main(String[] args) {
+Carafe c1, c2;
+		
+		c1 = new Carafe(4);
+		c2 = new Carafe(7);
+		
+		
+		
+
+	}
+
+}
