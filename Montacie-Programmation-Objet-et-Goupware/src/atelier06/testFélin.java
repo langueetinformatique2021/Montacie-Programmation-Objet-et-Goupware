e2f1fff56bfb6a08564fcec0de89c01fd9b86f3c
isha <isha@fedora>
2023-11-23 11:02:22 +0100

    Correction de testFélin

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/testFélin.java
@@ -0,0 +1,13 @@
+package atelier06;
+
+public class testFélin {
+
+	public static void main(String[] args) {
+		Félin f1 = new Félin("Lions");
+		f1.présente();
+		
+		Félin f2 = new Félin("Tigres");
+		f2.présente();
+	}
+
+}
