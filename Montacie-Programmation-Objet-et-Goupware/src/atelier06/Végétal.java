a518dc80844ad3e809aeed2e2bd9e8309b80311a
isha <isha@fedora>
2023-11-23 12:03:58 +0100

    Code ChatGpt

--- Montacie-Programmation-Objet-et-Goupware/src/atelier06/Végétal.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/Végétal.java
@@ -1,5 +1,14 @@
 package atelier06;
 
+/**
+Correction obtenue par CharGPT
+Soit l'arborescence de classe en Java Végétal Arbre Peuplier. 
+La classe Java Végétal représente un végétal. 
+La classe Arbre représente un arbre et dérive de la classe Végétal. 
+La classe Peuplier représente un peuplier et dérive de la classe Arbre. 
+Ecrire le code des trois classes.
+*/
+
 //Classe Végétal
 public class Végétal {
  // Attributs, méthodes, constructeurs, etc., propres à la classe Végétal
