0a1dd3ad78a0f57f3421ff447e5996ed0243f267
isha <isha@fedora>
2023-11-30 11:34:38 +0100

    Connaître l'heure courante

--- Montacie-Programmation-Objet-et-Goupware/src/atelier07/testGregorianCalendar.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/testGregorianCalendar.java
@@ -6,6 +6,7 @@ public class testGregorianCalendar {
 
 	public static void main(String[] args) {
 		GregorianCalendar gc = new GregorianCalendar();
+		
 		Date d = gc.getTime();
 		System.out.println(d.toString());
 
---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/testGregorianCalendar2.java
@@ -0,0 +1,24 @@
+package atelier07;
+
+import java.util.*;
+
+public class testGregorianCalendar2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		GregorianCalendar gc = new GregorianCalendar();
+		
+		String str = sc.nextLine();
+		
+		// quelle est la date et l'heure à ce point du programme
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
