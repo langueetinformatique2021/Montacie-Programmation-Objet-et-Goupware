a312e5648904b8ea9ecaed5cf6b100a80a5864e5
isha <isha@fedora>
2023-12-21 10:21:13 +0100

    Copie - lecture et écriture

--- Montacie-Programmation-Objet-et-Goupware/src/atelier09/Copie.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/Copie.java
@@ -1,14 +1,26 @@
 package atelier09;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.FileOutputStream;
 
 public class Copie {
 
 	public static void main(String[] args) {
 		File fent = new File("Noms.txt");
 		System.out.println(fent.length());
+		try {
+		FileInputStream f1 = new FileInputStream(fent);
+		System.out.println(f1.available());
 		
-
+		FileOutputStream f2 = new FileOutputStream (new File ("Nomscopie.txt"));
+		
+		f1.close();
+		f2.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
