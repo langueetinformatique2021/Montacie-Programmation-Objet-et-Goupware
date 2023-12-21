b97eb1e521b3003c5e2075ec10a0e5a635c8641d
isha <isha@fedora>
2023-12-21 09:59:06 +0100

    Copie lecture de la taille du fichier

--- Montacie-Programmation-Objet-et-Goupware/src/atelier09/Copie.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/Copie.java
@@ -1,8 +1,12 @@
 package atelier09;
 
+import java.io.File;
+
 public class Copie {
 
 	public static void main(String[] args) {
+		File fent = new File("Noms.txt");
+		System.out.println(fent.length());
 		
 
 	}
