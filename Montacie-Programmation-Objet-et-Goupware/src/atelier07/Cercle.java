49e51ded036360f9eebec7ef233b7812243da714
masterlangueetinformatique <masterlangueetinformatique@vbox>
2024-11-28 11:44:09 +0100

    Ajout du constructeur de Cercle

--- Montacie-Programmation-Objet-et-Goupware/src/atelier07/Cercle.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier07/Cercle.java
@@ -4,5 +4,13 @@ import cours04.FigureGeometrique;
 import java.lang.Comparable;
 
 public class Cercle extends FigureGeometrique implements Comparable  {
+	
+	double rayon;
+	
+	/** Création d'une nouvelle instance de Cercle */
+	public Cercle(float x) {
+		super("noir");
+		rayon = x;
+	}
 
 }
