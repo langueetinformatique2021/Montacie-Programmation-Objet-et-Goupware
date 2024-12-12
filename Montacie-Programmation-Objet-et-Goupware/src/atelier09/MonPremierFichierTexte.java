5e7d0f03f8430fe4327ddd87c0f2f88c1784063d
masterlangueetinformatique <masterlangueetinformatique@vbox>
2024-12-12 11:49:58 +0100

    Ecriture dans un fichier

---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/MonPremierFichierTexte.java
@@ -0,0 +1,10 @@
+package atelier09;
+
+public class MonPremierFichierTexte {
+	/** point d'entrée d'un exécutable */
+	public static void main(String[] args) {
+		int num[] = { 21402871, 21104591, 21401764, 21408349, 21314076, 21109469, 21202064, 21415136, 21410005,
+				21415557, 21200439, 21407911, 21101218, 28607925, 21400908, 21314636 };
+	}
+
+}
