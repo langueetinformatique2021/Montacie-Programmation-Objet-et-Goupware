bb1ab4ec53e61219f2969a8d5d23f81461fa2358
masterlangueinformatique <masterlangueinformatique@fedora>
2025-11-27 10:37:42 +0100

    Corrigé partiel atelier 6

--- Montacie-Programmation-Objet-et-Goupware/src/atelier06/Bronze.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/Bronze.java
@@ -2,6 +2,14 @@ package atelier06;
 
 public class Bronze extends Metal {
 
+	/**
+	 * création d'une nouvelle instance de la classe Bronze
+	 * 
+	 */
+	public Bronze() {
+		super("Bronze", 890, 75);
+	}
+
 	@Override
 	public String instruments() {
 		// TODO Auto-generated method stub
---
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/testBronze.java
@@ -0,0 +1,11 @@
+package atelier06;
+
+public class testBronze {
+
+	public static void main(String[] args) {
+		Bronze m = new Bronze();
+		m.afficher();
+
+	}
+
+}
