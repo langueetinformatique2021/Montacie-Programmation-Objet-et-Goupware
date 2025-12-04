70106c40e6e6e7945f0dbd934cb4481cddbd076c
masterlangueinformatique <masterlangueinformatique@fedora>
2025-12-04 10:01:33 +0100

    correction

--- Montacie-Programmation-Objet-et-Goupware/src/atelier06/Bronze.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/Bronze.java
@@ -1,6 +1,7 @@
 package atelier06;
 
 public class Bronze extends Metal {
+	private String instruments;
 
 	/**
 	 * création d'une nouvelle instance de la classe Bronze
@@ -8,12 +9,18 @@ public class Bronze extends Metal {
 	 */
 	public Bronze() {
 		super("Bronze", 890, 75);
+		this.instruments = "marteau, maillet, clé, statue, vase, bougeoir, encrier, coupe, pendule, lampe, charrue";
 	}
-
+	
+	/** présentation des caractéristiques du Bronze */
+	public void afficher() {
+		super.afficher();
+		System.out.println("je peux servir les instruments suivants: " + instruments()); 
+	}
+	
 	@Override
 	public String instruments() {
-		// TODO Auto-generated method stub
-		return null;
+		return instruments;
 	}
 
 }
--- Montacie-Programmation-Objet-et-Goupware/src/atelier06/Charrue.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier06/Charrue.java
@@ -1,5 +1,18 @@
 package atelier06;
 
+/** outil aratoire à structure dissymétrique */
+
 public class Charrue extends Bronze {
+	private String definition;
+	private String invention;
+	private String inventeur;
+	
+	public Charrue() {
+		this.definition = "outil aratoire à structure dissymétrique";
+		this.invention = "IV millénaire av. J.-C";
+		this.inventeur = "Summer";
+	}
+	
+	
 
 }
