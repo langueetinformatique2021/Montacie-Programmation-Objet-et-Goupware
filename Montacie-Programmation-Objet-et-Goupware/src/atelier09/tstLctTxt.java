9020ca82c74864d335771a288c27c0b1e13c0d56
masterlangueinformatique <masterlangueinformatique@fedora>
2025-12-11 11:58:30 +0100

    Correction

--- Montacie-Programmation-Objet-et-Goupware/src/atelier09/tstLctTxt.java
+++ Montacie-Programmation-Objet-et-Goupware/src/atelier09/tstLctTxt.java
@@ -3,20 +3,18 @@ import java.io.*;
 import util.Keyboard;
 public class tstLctTxt {
 	public static void main(String[] args) {
-		int num[] = { 21402871, 21104591, 21401764, 21408349, 21314076, 21109469, 21202064, 21415136, 21410005,
-				21415557, 21200439, 21407911, 21101218, 28607925, 21400908, 21314636 };
-
 		String ligne;
-		int numLigne = 0;
+
 		try {
-			File f = new File("Nom.txt");
+			File f = new File("Montacie-Programmation-Objet-et-Goupware/src/atelier09/Nom.txt");
 			FileReader fr = new FileReader(f);
 			BufferedReader br = new BufferedReader(fr);
 			while ((ligne=br.readLine()) != null) {
-				System.out.println(ligne + " " + num[numLigne++]); 
+				System.out.println(ligne); 
 			}
 			br.close();
 		} 
+		catch (FileNotFoundException e) { e.printStackTrace(); }
 		catch (IOException e) { e.printStackTrace(); }
 	} 
 }
